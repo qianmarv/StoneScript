f9fbac42accd929d2ec15a0203b8369330ca082b
Marvin Qian <qianmarv@gmail.com>
2019-08-10 20:08:53 +0800

    Fix Punct Issue

--- stone/Lexer.java
+++ stone/Lexer.java
@@ -10,7 +10,7 @@ import java.util.regex.Pattern;
 public class Lexer {
     public static String regexPat
         = "\\s*((//.*)|([0-9]+)|(\"(\\\\\"|\\\\\\\\|\\\\n|[^\"])*\")"
-        + "|[A-Z_a-z][A-Z_a-z_0-9]*|==|<=|>=|&&|\\|\\||\\p{Puct})?";
+        + "|[A-Z_a-z][A-Z_a-z_0-9]*|==|<=|>=|&&|\\|\\||\\p{Punct})?";
     private Pattern pattern = Pattern.compile(regexPat);
     private ArrayList<Token> queue = new ArrayList<Token>();
     private boolean hasMore;
